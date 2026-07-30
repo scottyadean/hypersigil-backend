98f26555c3c0e1667dceedbfa6a1ae2a7806184b
scottadean <scott@nativeamericantours.com>
2026-07-29 18:16:57 -0700

    removed config files

--- server.sh
+++ server.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+
+echo "Loading any env vars from .env"
+eval "$(
+  cat .env | awk '!/^\s*#/' | awk '!/^\s*$/' | while IFS='' read -r line; do
+    key=$(echo "$line" | cut -d '=' -f 1)
+    value=$(echo "$line" | cut -d '=' -f 2-)
+    echo "export $key=\"$value\""
+  done
+)"
+
+
 find . -name "*.pyc" -exec rm -f {} \;
 export AWS_PROFILE=dev-lagunacreek;
 sls offline --stage local --noAuth --httpPort 5500 --reloadHandler;
